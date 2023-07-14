38dbb47f04ca065f90839b6e442850192023d7d9
Ben Best <ben@ecoquants.com>
2023-07-14 13:22:07 +0200

    cp dir_pw/*.docx -> dir_html/*.docx

--- scripts/pw_mv.R
+++ scripts/pw_mv.R
@@ -7,6 +7,7 @@ dir_html      <- here("_book")
 dir_pw        <- glue("{dir_html}/44ce7cff6e9a3ce81a45412598fcb96fcd5108bc")
 
 # TODO: move dir_html/* -> dir_pw/*
+#       cp dir_pw/*.docx -> dir_html/*.docx
 
 for (file_html in list.files(dir_pw, ".*\\.html$")){ # path_dest = dir_ls(dir_pw, glob="*.html")[1]
 
